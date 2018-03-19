dc93a519d2afd017416bdd343aad143408f2be41
lucvaladao <lucasvaladao@getinfo.net.br>
2018-03-19 20:04:11 -0300

    Instância de Banco usando Application

---
+++ app/src/main/java/com/example/lucvaladao/entrevistapopcode/db/MyApp.java
@@ -0,0 +1,19 @@
+package com.example.lucvaladao.entrevistapopcode.db;
+
+import android.app.Application;
+import android.arch.persistence.room.Room;
+
+/**
+ * Created by lucvaladao on 3/19/18.
+ */
+
+public class MyApp extends Application {
+
+    private static AppDatabase database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        database = Room.databaseBuilder(this, AppDatabase.class, "roomDB").build();
+    }
+}
