aa8fc785b5320f2d7cf975618d11f2fd7a529f98
Jayson Sabino <jaysonsabino01@gmail.com>
2018-09-05 20:28:21 -0300

    Testando a recycler view

--- app/src/main/java/br/com/jaysonsabino/desafioandroidpopcode/MainActivity.java
+++ app/src/main/java/br/com/jaysonsabino/desafioandroidpopcode/MainActivity.java
@@ -1,7 +1,12 @@
 package br.com.jaysonsabino.desafioandroidpopcode;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> itens = Arrays.asList("Teste", "Jayson");
+
+        RecyclerView lista = findViewById(R.id.main_lista_personagens);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        lista.setLayoutManager(layoutManager);
+
+        lista.setAdapter(new ListaPersonagensAdapter(this, itens));
     }
 }
