f8c7b9e97007b3b07dd045b1ec5f9910ccd989ad
Matheus Fróes <matheusfroesmarques@gmail.com>
2018-09-11 13:59:40 -0300

    PersonDetailViewModel accessing AppRepository and UI <observes> ViewModel

--- app/src/main/java/com/matheusfroes/swapi/ui/persondetail/PersonDetailActivity.kt
+++ app/src/main/java/com/matheusfroes/swapi/ui/persondetail/PersonDetailActivity.kt
@@ -1,24 +1,80 @@
 package com.matheusfroes.swapi.ui.persondetail
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.matheusfroes.swapi.R
-import com.matheusfroes.swapi.appInjector
-import com.matheusfroes.swapi.viewModelProvider
+import android.util.Log
+import android.view.View
+import com.matheusfroes.swapi.*
+import com.matheusfroes.swapi.data.model.Person
+import kotlinx.android.synthetic.main.activity_person_detail.*
 import javax.inject.Inject
 
 class PersonDetailActivity : AppCompatActivity() {
+    companion object {
+        const val PERSON_ID = "com.matheusfroes.swapi.person_id"
+    }
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: PersonDetailViewModel
+
+    private val personId: Long by lazy { intent.getLongExtra(PERSON_ID, 0) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_person_detail)
         appInjector.inject(this)
 
+        intent ?: return
+
         viewModel = viewModelProvider(viewModelFactory)
+        viewModel.personId = personId
+
+        viewModel.personDetailObservable.observe(this, Observer { result ->
+            when (result) {
+                is Result.Complete -> {
+                    updateUI(result.data)
+                    hideLoadingIndicator()
+                }
+                is Result.InProgress -> {
+                    showLoadingIndicator()
+                }
+                is Result.Error -> {
+                    Log.e("SWAPI", "Erro ao exibir personagem", result.error)
+                    hideLoadingIndicator()
+                }
+            }
+        })
+
+        btnBookmarkPerson.setOnClickListener {
+            viewModel.toggleBookmarkPerson()
+        }
+
+        viewModel.bookmarkedPersonEvent.observe(this, Observer { bookmarkMessage ->
+            toast(bookmarkMessage)
+        })
+    }
+
+    private fun updateUI(person: Person) {
+        tvName.text = person.name
+        tvHeight.text = person.height
+        tvMass.text = person.mass
+        tvHairColor.text = person.hairColor
+        tvSkinColor.text = person.skinColor
+        tvEyeColor.text = person.eyeColor
+        tvBirthYear.text = person.birthYear
+        tvGender.text = person.gender
+        tvHomeworld.text = person.homeworld
+        tvSpecies.text = person.species
+    }
+
+    private fun showLoadingIndicator() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoadingIndicator() {
+        progressBar.visibility = View.GONE
     }
 }
--- app/src/main/java/com/matheusfroes/swapi/ui/persondetail/PersonDetailViewModel.kt
+++ app/src/main/java/com/matheusfroes/swapi/ui/persondetail/PersonDetailViewModel.kt
@@ -1,9 +1,50 @@
 package com.matheusfroes.swapi.ui.persondetail
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.matheusfroes.swapi.Result
+import com.matheusfroes.swapi.SingleLiveEvent
+import com.matheusfroes.swapi.data.AppRepository
+import com.matheusfroes.swapi.data.model.Person
+import com.matheusfroes.swapi.uiContext
+import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
 
-class PersonDetailViewModel @Inject constructor(
+typealias BookmarkedMessage = String
 
+class PersonDetailViewModel @Inject constructor(
+        private val repository: AppRepository
 ) : ViewModel() {
+    private val _personDetailObservable = MutableLiveData<Result<Person>>()
+    val personDetailObservable: LiveData<Result<Person>> = _personDetailObservable
+
+    val bookmarkedPersonEvent = SingleLiveEvent<BookmarkedMessage>()
+
+    lateinit var person: Person
+    var personId: Long = 0
+        set(value) {
+            field = value
+            getPerson(personId)
+        }
+
+    private fun getPerson(personId: Long) = launch(uiContext) {
+        _personDetailObservable.value = Result.InProgress()
+
+        try {
+            person = repository.getPerson(personId)
+
+            _personDetailObservable.value = Result.Complete(person)
+        } catch (e: Exception) {
+            _personDetailObservable.value = Result.Error(e)
+        }
+    }
+
+    fun toggleBookmarkPerson() = launch(uiContext) {
+        if (person.isBookmarked) {
+            repository.unbookmarkPerson(personId)
+        } else {
+            repository.bookmarkPerson(personId)
+        }
+    }
 }
