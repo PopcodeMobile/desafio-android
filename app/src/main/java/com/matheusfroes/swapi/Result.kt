da933e69910ef1a03fcbc9d543aebde86a3d95ff
Matheus Fróes <matheusfroesmarques@gmail.com>
2018-09-11 14:00:03 -0300

    Utility classes Result and SingleLiveEvent

---
+++ app/src/main/java/com/matheusfroes/swapi/Result.kt
@@ -0,0 +1,11 @@
+package com.matheusfroes.swapi
+
+sealed class Result<D> {
+
+    class InProgress<D>(val cachedData: D? = null) : Result<D>()
+
+    data class Complete<D>(val data: D) : Result<D>()
+
+    data class Error<D>(val error: Throwable? = null) : Result<D>()
+
+}
