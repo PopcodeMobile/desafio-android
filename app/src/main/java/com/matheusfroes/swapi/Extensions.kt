c1610cad7436a98aa63a5d4e93d110ac2499336a
Matheus Fróes Marques <matheusfroesmarques@gmail.com>
2018-09-10 22:19:11 -0300

    Added app extensions

---
+++ app/src/main/java/com/matheusfroes/swapi/Extensions.kt
@@ -0,0 +1,49 @@
+package com.matheusfroes.swapi
+
+import android.app.Activity
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.net.Uri
+import android.support.v4.app.FragmentActivity
+import com.matheusfroes.swapi.di.Injector
+import kotlinx.coroutines.experimental.async
+import kotlin.coroutines.experimental.CoroutineContext
+
+val Activity.app: SwapiApplication get() = application as SwapiApplication
+val Activity.appInjector: Injector get() = app.injector
+
+typealias NextPageUrl = String?
+
+fun extractIdFromUrl(url: String): Long {
+    val uri = Uri.parse(url)
+    val id = uri.lastPathSegment ?: "0"
+    return id.toLong()
+}
+
+/**
+ * For Activities, allows declarations like
+ * ```
+ * val myViewModel = viewModelProvider(myViewModelFactory)
+ * ```
+ */
+inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(
+        provider: ViewModelProvider.Factory
+) =
+        ViewModelProviders.of(this, provider).get(VM::class.java)
+
+
+suspend fun <A, B> List<A>.parallelMap(
+        context: CoroutineContext = networkContext,
+        block: suspend (A) -> B
+): List<B> {
+    return map {
+        // Use async to start a coroutine for each item
+        async(context) {
+            block(it)
+        }
+    }.map {
+        // We now have a map of Deferred<T> so we await() each
+        it.await()
+    }
+}
