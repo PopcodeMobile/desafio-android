1b81c8b7dc3aa47c47fb6d628ea679a48a84eebf
ThiagoFontes <thiago93br@gmail.com>
2019-05-22 10:44:51 -0300

    Adding Person class and dependency jackson-databind

---
+++ app/src/main/java/com/example/starwarswiki/structural/Person.java
@@ -0,0 +1,161 @@
+package com.example.starwarswiki.structural;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "name",
+        "height",
+        "mass",
+        "hair_color",
+        "skin_color",
+        "eye_color",
+        "birth_year",
+        "gender",
+        "homeworld",
+        "species"
+})
+public class Person {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("height")
+    private String height;
+    @JsonProperty("mass")
+    private String mass;
+    @JsonProperty("hair_color")
+    private String hairColor;
+    @JsonProperty("skin_color")
+    private String skinColor;
+    @JsonProperty("eye_color")
+    private String eyeColor;
+    @JsonProperty("birth_year")
+    private String birthYear;
+    @JsonProperty("gender")
+    private String gender;
+    @JsonProperty("homeworld")
+    private String homeworld;
+    @JsonProperty("species")
+    private List<String> species = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("height")
+    public String getHeight() {
+        return height;
+    }
+
+    @JsonProperty("height")
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    @JsonProperty("mass")
+    public String getMass() {
+        return mass;
+    }
+
+    @JsonProperty("mass")
+    public void setMass(String mass) {
+        this.mass = mass;
+    }
+
+    @JsonProperty("hair_color")
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    @JsonProperty("hair_color")
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    @JsonProperty("skin_color")
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    @JsonProperty("skin_color")
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    @JsonProperty("eye_color")
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    @JsonProperty("eye_color")
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    @JsonProperty("birth_year")
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    @JsonProperty("birth_year")
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    @JsonProperty("gender")
+    public String getGender() {
+        return gender;
+    }
+
+    @JsonProperty("gender")
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @JsonProperty("homeworld")
+    public String getHomeworld() {
+        return homeworld;
+    }
+
+    @JsonProperty("homeworld")
+    public void setHomeworld(String homeworld) {
+        this.homeworld = homeworld;
+    }
+
+    @JsonProperty("species")
+    public List<String> getSpecies() {
+        return species;
+    }
+
+    @JsonProperty("species")
+    public void setSpecies(List<String> species) {
+        this.species = species;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
