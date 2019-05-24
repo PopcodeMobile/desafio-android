88dd1b660fb22a7bac919d2cf21e427db5d365df
ThiagoFontes <thiago93br@gmail.com>
2019-05-24 03:32:25 -0300

    AndroidX migration

--- app/src/main/java/com/example/starwarswiki/MainActivity.java
+++ app/src/main/java/com/example/starwarswiki/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.starwarswiki;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
--- app/src/main/java/com/example/starwarswiki/structural/Person.java
+++ app/src/main/java/com/example/starwarswiki/structural/Person.java
@@ -1,14 +1,17 @@
 package com.example.starwarswiki.structural;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import androidx.room.PrimaryKey;
+
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -24,7 +27,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
         "species"
 })
 public class Person {
-
+    @PrimaryKey(autoGenerate = true)
+    private int id;
     @JsonProperty("name")
     private String name;
     @JsonProperty("height")
