49ba5cb1a68a460e1db93eb74617d939fb2171f1
Joao <joaootaviobf21@gmail.com>
2019-06-24 19:26:59 -0300

    bottom navigation

---
+++ app/src/main/java/com/example/entrevistapopcode/views/fragments/FavoriteFragment.java
@@ -0,0 +1,6 @@
+package com.example.entrevistapopcode.views.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class FavoriteFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/entrevistapopcode/views/fragments/HomeFragment.java
@@ -0,0 +1,6 @@
+package com.example.entrevistapopcode.views.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+}
