266e234723f80b3a5933ca320905aadb7df3da55
Rodrigo Otsuka <rodrigo.otsuka@dcomp.ufs.br>
2019-11-23 14:26:16 -0300

    feat: add Room database

---
+++ app/src/main/java/com/example/starwarswiki/database/Room.kt
@@ -0,0 +1,26 @@
+package com.example.starwarswiki.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [DatabasePerson::class], version = 1, exportSchema = false)
+abstract class PersonRoomDatabase: RoomDatabase(){
+    abstract val personDao: PersonDao
+}
+
+private lateinit var INSTANCE: PersonRoomDatabase
+
+fun getDatabase(context: Context): PersonRoomDatabase{
+    synchronized(PersonRoomDatabase::class.java){
+        if (!::INSTANCE.isInitialized){
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                PersonRoomDatabase::class.java,
+                "persons"
+            ).build()
+        }
+    }
+    return INSTANCE
+}
