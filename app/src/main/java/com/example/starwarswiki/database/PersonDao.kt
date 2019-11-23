81b8b884a8814e012ae21b4a29748821bdaa1794
Rodrigo Otsuka <rodrigo.otsuka@dcomp.ufs.br>
2019-11-23 14:25:24 -0300

    feat: add data acess object (DAO) that allows fetch and insert data to Room.

---
+++ app/src/main/java/com/example/starwarswiki/database/PersonDao.kt
@@ -0,0 +1,16 @@
+package com.example.starwarswiki.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface PersonDao {
+    @Query("SELECT * FROM databaseperson")
+    fun getPersons(): LiveData<List<DatabasePerson>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(persons: List<DatabasePerson>)
+}
+
