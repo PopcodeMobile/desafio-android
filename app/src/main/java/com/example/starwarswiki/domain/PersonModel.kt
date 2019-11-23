68b6b0d785bdc18a5996ffc9d82323f625cf2333
Rodrigo Otsuka <rodrigo.otsuka@dcomp.ufs.br>
2019-11-23 14:27:13 -0300

    feat: add Person model

---
+++ app/src/main/java/com/example/starwarswiki/domain/PersonModel.kt
@@ -0,0 +1,15 @@
+package com.example.starwarswiki.domain
+
+data class PersonModel(
+    val url: String,
+    val name: String,
+    val height: String,
+    val mass: String,
+    val hair_color: String,
+    val skin_color: String,
+    val eye_color: String,
+    val birth_year: String,
+    val gender: String,
+    val homeworld: String,
+    val species: List<String>
+)
