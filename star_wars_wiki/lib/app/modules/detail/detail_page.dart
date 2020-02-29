d4a20fcfd1132e5f95ef305067cf422a0bc01e27
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 19:04:37 -0300

    Blocos de informação estilizados

--- star_wars_wiki/lib/app/modules/detail/detail_page.dart
+++ star_wars_wiki/lib/app/modules/detail/detail_page.dart
@@ -1,27 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:star_wars_wiki/app/modules/detail/custom_card.dart';
+import 'package:star_wars_wiki/app/modules/home/home_bloc.dart';
+import 'package:star_wars_wiki/app/modules/home/home_module.dart';
 import 'package:star_wars_wiki/shared/models/character_model.dart';
 
-class DetailPage extends StatefulWidget {
-  final CharacterModel char;
+import 'detail_bloc.dart';
 
-  const DetailPage({Key key, @required this.char}) : super(key: key);
+class DetailPage extends StatefulWidget {
   @override
   _DetailPageState createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
+  String planet = '', specie = '';
+  final bloc = HomeModule.to.getBloc<DetailBloc>();
+  final homeBloc = HomeModule.to.getBloc<HomeBloc>();
+  CharacterModel char;
+
+  @override
+  void initState() {
+    setState(() {
+      char = homeBloc.selectedChar;
+      bloc.fetchInfo(planetPath: char.homeworld, species: char.species);
+      bloc.responseIn.add(null);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          widget.char.name,
+          homeBloc.selectedChar.name.toLowerCase(),
           style: TextStyle(
             fontFamily: 'Star Jedi',
-            // fontSize: 25.0,
+            fontSize: 24.0,
           ),
         ),
         centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.black38,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          children: <Widget>[
+            CustomCard(
+              label: 'Name',
+              text: char.name,
+            ),
+            CustomCard(
+              label: 'Height',
+              text:
+                  char.height == 'unknown' ? char.height : '${char.height} cm',
+            ),
+            CustomCard(
+              label: 'Mass',
+              text: char.mass == 'unknown' ? char.mass : '${char.mass} kg',
+            ),
+            CustomCard(
+              label: 'Hair',
+              text: char.hairColor,
+            ),
+            CustomCard(
+              label: 'Skin Color',
+              text: char.skinColor,
+            ),
+            CustomCard(
+              label: 'Birth Year',
+              text: char.birthYear,
+            ),
+            CustomCard(
+              label: 'Gender',
+              text: char.gender,
+            ),
+            StreamBuilder(
+                stream: bloc.responseOut,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text(snapshot.error.toString()));
+                  }
+
+                  if (snapshot.hasData) {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                            CustomCard(
+                              label: 'Home World',
+                              text: bloc.planet.name,
+                            ),
+                          ] +
+                          [
+                            CustomCard(
+                              label: 'Species',
+                              text: bloc.speciesList.isNotEmpty
+                                  ? bloc.speciesList[0].name
+                                  : '',
+                            )
+                          ] +
+                          bloc.speciesList
+                              .skip(1)
+                              .map(
+                                (specie) => CustomCard(
+                                  text: specie.name,
+                                ),
+                              )
+                              .toList(),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                }),
+          ],
+        ),
       ),
     );
   }
