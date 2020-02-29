73954c6f0a63ec72313a5377f95d7feb750476e6
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 15:13:26 -0300

    Model de personagem com contrutor nomeado fromJson e função de mapeamento toJson

---
+++ star_wars_wiki/lib/shared/models/character_model.dart
@@ -0,0 +1,50 @@
+class CharacterModel {
+  String name,
+      height,
+      mass,
+      hairColor,
+      skinColor,
+      eyeColor,
+      birthYear,
+      gender,
+      homeworld,
+      url;
+  List<String> films, species, vehicles, starships;
+  bool fav = false;
+
+  CharacterModel.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    height = json['height'];
+    mass = json['mass'];
+    hairColor = json['hair_color'];
+    skinColor = json['skin_color'];
+    eyeColor = json['eye_color'];
+    birthYear = json['birth_year'];
+    gender = json['gender'];
+    homeworld = json['homeworld'];
+    films = json['films'].cast<String>();
+    species = json['species'].cast<String>();
+    vehicles = json['vehicles'].cast<String>();
+    starships = json['starships'].cast<String>();
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['height'] = this.height;
+    data['mass'] = this.mass;
+    data['hair_color'] = this.hairColor;
+    data['skin_color'] = this.skinColor;
+    data['eye_color'] = this.eyeColor;
+    data['birth_year'] = this.birthYear;
+    data['gender'] = this.gender;
+    data['homeworld'] = this.homeworld;
+    data['films'] = this.films;
+    data['species'] = this.species;
+    data['vehicles'] = this.vehicles;
+    data['starships'] = this.starships;
+    data['url'] = this.url;
+    return data;
+  }
+}
