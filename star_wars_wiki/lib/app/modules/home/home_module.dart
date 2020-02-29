a0d3e27c2a4b3caf66ae90e9ebee967b10609e05
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 15:08:40 -0300

    Módulo Home com injeção de dependências do Repository

---
+++ star_wars_wiki/lib/app/modules/home/home_module.dart
@@ -0,0 +1,26 @@
+import 'package:star_wars_wiki/app/modules/home/home_bloc.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:star_wars_wiki/app/modules/home/home_page.dart';
+import 'package:star_wars_wiki/app/modules/home/home_repository.dart';
+import 'package:star_wars_wiki/shared/custom_dio/custom_dio.dart';
+
+import '../../app_module.dart';
+
+class HomeModule extends ModuleWidget {
+  @override
+  List<Bloc> get blocs => [
+        Bloc((i) => HomeBloc(HomeModule.to.getDependency<HomeRepository>())),
+      ];
+
+  @override
+  List<Dependency> get dependencies => [
+        Dependency(
+            (i) => HomeRepository(AppModule.to.getDependency<CustomDio>()))
+      ];
+
+  @override
+  Widget get view => HomePage();
+
+  static Inject get to => Inject<HomeModule>.of();
+}
