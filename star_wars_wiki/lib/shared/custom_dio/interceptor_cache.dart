78a9a69248ffc11006e2bac8be377292fe7301de
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 15:17:09 -0300

    Cacheando com Shared Preferences

---
+++ star_wars_wiki/lib/shared/custom_dio/interceptor_cache.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CaheInterceptor extends InterceptorsWrapper {
+  @override
+  onRequest(RequestOptions options) async {
+    return options;
+  }
+
+  @override
+  onResponse(Response response) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("${response.request.uri}", jsonEncode(response.data));
+    return response;
+  }
+
+  @override
+  onError(DioError error) async {
+    if (error.type == DioErrorType.CONNECT_TIMEOUT ||
+        error.type == DioErrorType.DEFAULT) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var res = jsonDecode(prefs.getString('${error.request.uri}'));
+      return Response(data: res, statusCode: 200);
+    } else {
+      return error;
+    }
+  }
+}
