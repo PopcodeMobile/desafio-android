b3c188281e01270c641d3602ae601d975d8b0a9d
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 19:02:30 -0300

    Adicionadas funções de obter Espécie e Planeta

--- star_wars_wiki/lib/app/modules/home/home_repository.dart
+++ star_wars_wiki/lib/app/modules/home/home_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:star_wars_wiki/shared/custom_dio/custom_dio.dart';
 import 'package:star_wars_wiki/shared/models/character_model.dart';
+import 'package:star_wars_wiki/shared/models/planet_model.dart';
+import 'package:star_wars_wiki/shared/models/specie_model.dart';
 
 class HomeRepository {
   final CustomDio _client;
@@ -13,6 +15,24 @@ class HomeRepository {
       return (response.data['results'] as List)
           .map((item) => CharacterModel.fromJson(item))
           .toList();
+    } on DioError catch (e) {
+      throw (e.message);
+    }
+  }
+
+  Future<PlanetModel> getPlanet(String path) async {
+    try {
+      var response = await _client.get(path);
+      return PlanetModel.fromJson(response.data);
+    } on DioError catch (e) {
+      throw (e.message);
+    }
+  }
+
+  Future<SpecieModel> getSpecie(path) async {
+    try {
+      var response = await _client.get(path);
+      return SpecieModel.fromJson(response.data);
     } on DioError catch (e) {
       throw (e.message);
     }
