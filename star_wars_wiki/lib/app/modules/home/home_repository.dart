73347d299aafedef668be804991f535e4b9e79e0
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 22:34:40 -0300

    Adicionadas funções de manipular favoritos (salvar, remover e checar)

--- star_wars_wiki/lib/app/modules/home/home_repository.dart
+++ star_wars_wiki/lib/app/modules/home/home_repository.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:star_wars_wiki/shared/constants.dart';
 import 'package:star_wars_wiki/shared/custom_dio/custom_dio.dart';
 import 'package:star_wars_wiki/shared/models/character_model.dart';
 import 'package:star_wars_wiki/shared/models/planet_model.dart';
@@ -11,10 +15,15 @@ class HomeRepository {
 
   Future<List<CharacterModel>> getCharacters({int page: 1}) async {
     try {
-      var response = await _client.get('/people/?page=$page');
-      return (response.data['results'] as List)
+      var response = await _client.get('$BASE_URL/people/?page=$page');
+      var charList = (response.data['results'] as List)
           .map((item) => CharacterModel.fromJson(item))
           .toList();
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      for (var char in charList) {
+        if (prefs.containsKey('favorites/${char.name}')) char.fav = true;
+      }
+      return charList;
     } on DioError catch (e) {
       throw (e.message);
     }
@@ -22,7 +31,7 @@ class HomeRepository {
 
   Future<PlanetModel> getPlanet(String path) async {
     try {
-      var response = await _client.get(path);
+      var response = await _client.get('$BASE_URL$path');
       return PlanetModel.fromJson(response.data);
     } on DioError catch (e) {
       throw (e.message);
@@ -31,10 +40,42 @@ class HomeRepository {
 
   Future<SpecieModel> getSpecie(path) async {
     try {
-      var response = await _client.get(path);
+      var response = await _client.get('$BASE_URL$path');
       return SpecieModel.fromJson(response.data);
     } on DioError catch (e) {
       throw (e.message);
     }
+  }
+
+  Future<String> setFavorite(String id) async {
+    String message;
+    try {
+      var response = await _client.post(
+        '$FAVS_BASE_URL/favorite/$id',
+      );
+      message = response.data['message'];
+    } on DioError catch (e) {
+      throw (e.message);
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("favorites/$id", id);
+    return message;
+  }
+
+  Future<String> removeFavorite(String id) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey('favorites/$id')) {
+      if (await prefs.remove('favorites/$id'))
+        return 'Removido dos favoritos';
+      else
+        return 'Erro ao remover dos favoritos';
+    } else {
+      return 'Você não deveria estar vendo isso';
+    }
+  }
+
+  Future<bool> isFavorite(String id) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.containsKey('favorites/$id');
   }
 }
