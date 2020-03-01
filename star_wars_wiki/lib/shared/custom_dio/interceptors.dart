97e8d7b473ffa45a551c1ff0d4880d80f55b5004
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 22:30:01 -0300

    Enviando o header Prefer com o status=400 em metade das requisições

--- star_wars_wiki/lib/shared/custom_dio/interceptors.dart
+++ star_wars_wiki/lib/shared/custom_dio/interceptors.dart
@@ -1,15 +1,23 @@
 import 'package:dio/dio.dart';
 
 class CustomInterceptors extends InterceptorsWrapper {
+  static int count = 0;
   @override
   onRequest(RequestOptions options) {
     print('REQUEST(${options.method}) => PATH: ${options.path}');
+    if ((count++) % 2 == 1) {
+      options.headers = {
+        "Prefer": {'status': 400}
+      };
+    }
+    print('Request Headers: ${options.headers}');
     return options;
   }
 
   @override
   onResponse(Response response) {
     print('RESPONSE(${response.statusCode}) => PATH: ${response.request.path}');
+    //print('RESPONSE => ${response.data}');
     return response;
   }
 
