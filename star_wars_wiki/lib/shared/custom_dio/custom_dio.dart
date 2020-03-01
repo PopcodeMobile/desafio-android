07559739d61cdc1511d1fbc0108b511021389c09
João Pedro Quintino <joao.quin@hotmail.com>
2020-02-29 22:31:35 -0300

    Removida a BASE URL do construtor

--- star_wars_wiki/lib/shared/custom_dio/custom_dio.dart
+++ star_wars_wiki/lib/shared/custom_dio/custom_dio.dart
@@ -1,12 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:star_wars_wiki/shared/constants.dart';
 import 'package:star_wars_wiki/shared/custom_dio/interceptors.dart';
 
 import 'interceptor_cache.dart';
 
 class CustomDio extends Dio {
   CustomDio() {
-    options.baseUrl = BASE_URL;
     interceptors.add(CustomInterceptors());
     interceptors.add(CacheInterceptor());
     options.connectTimeout = 5000;
