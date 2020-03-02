b97211b767c717b2378803e11e837550753f8a4a
Erlon Jr <ejrgeek@gmail.com>
2020-03-02 02:34:47 -0300

    [EDIT] Stylized Search Filters

--- wiki_starwars/lib/view/home.dart
+++ wiki_starwars/lib/view/home.dart
@@ -18,14 +18,17 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('WIKI Star Wars'),
+        title: Text('Wiki Star Wars'),
+        backgroundColor: Color(0xFF182E59),
       ),
       body: Container(
+        color: Color(0xFF0C1A35),
         padding: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
-            Text('Filtrar por:'),
+            Text('Filtrar por:', style: TextStyle(color: Colors.white),),
             SizedBox(height: 6,),
+
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -35,24 +38,35 @@ class _HomeState extends State<Home> {
                   height: 40,
                   child: TextField(
                       decoration: InputDecoration(
-                          hintStyle: TextStyle(fontSize: 12),
+                          hintStyle: TextStyle(fontSize: 12, color: Colors.white),
+                          labelStyle: TextStyle(color: Color(0xFF0387E9)),
                           border: OutlineInputBorder(),
                           suffixIcon: IconButton(
                             onPressed: (){},
+                            tooltip: 'Pesquisar',
                             icon: Icon(Icons.search),
+                            color: Color(0xFF0387E9),
                           ),
                           labelText: 'Pesquisar',
-                          hintText: 'Nome, planeta, especie')),
+                          hintText: 'Nome, planeta ou especie'
+                      )
+                  ),
                 ),
+
                 SizedBox(
                   child: IconButton(
                     tooltip: 'Favoritos',
                     onPressed: (){},
                     icon: Icon(Icons.star, color: Colors.yellow, size: 32,),
+                    hoverColor: Colors.yellowAccent,
+                    splashColor: Colors.yellowAccent,
                   ),
                 )
               ],
-            )
+            ),
+
+
+
           ],
         ),
       ),
