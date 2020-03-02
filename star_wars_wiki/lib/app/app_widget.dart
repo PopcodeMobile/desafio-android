29793d98ee75189e4a3f625f774dfbbc851e69d9
João Pedro Quintino <joao.quin@hotmail.com>
2020-03-02 18:24:27 -0300

    Adicionei uma Splash Screen

--- star_wars_wiki/lib/app/app_widget.dart
+++ star_wars_wiki/lib/app/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:star_wars_wiki/app/modules/home/home_module.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,7 +11,29 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: HomeModule(),
+      home: SplashScreen(
+        seconds: 5,
+        backgroundColor: Colors.orange.withGreen(140),
+        image: Image.asset('assets/images/bb8.gif'),
+        photoSize: 150,
+        navigateAfterSeconds: HomeModule(),
+        loaderColor: Colors.white,
+        title: Text(
+          'Star Wars Wiki',
+          style: TextStyle(
+            fontFamily: 'Star Jedi',
+            fontSize: 35,
+          ),
+        ),
+        loadingText: Text(
+          'May the force be with you',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Star Jedi',
+            fontSize: 18.0,
+          ),
+        ),
+      ),
     );
   }
 }
