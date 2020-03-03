922412bebed49c3d4da9645c383a84c4de276adc
João Pedro Quintino <joao.quin@hotmail.com>
2020-03-03 13:33:22 -0300

    Alterei a cor dos cards

--- star_wars_wiki/lib/app/modules/detail/custom_card_widget.dart
+++ star_wars_wiki/lib/app/modules/detail/custom_card_widget.dart
@@ -9,14 +9,14 @@ class CustomCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 60.0,
-      padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+      padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 5.0),
       child: Card(
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
         clipBehavior: Clip.hardEdge,
         color: Colors.transparent,
         child: Container(
-          color: Colors.yellow.withOpacity(0.5),
+          color: Colors.white70,
           padding: EdgeInsets.symmetric(horizontal: 10),
           child: Row(
             mainAxisSize: MainAxisSize.min,
@@ -24,14 +24,17 @@ class CustomCard extends StatelessWidget {
             children: <Widget>[
               Text(
                 label.isEmpty ? '' : '$label:',
-                style: TextStyle(fontSize: 20.0),
+                style: TextStyle(
+                  fontFamily: 'Times',
+                  fontSize: 22.0,
+                ),
               ),
               Expanded(flex: 1, child: SizedBox()),
               Text(
                 text,
                 style: TextStyle(
                   fontFamily: 'Times',
-                  fontSize: 20.0,
+                  fontSize: 22.0,
                 ),
               ),
             ],
