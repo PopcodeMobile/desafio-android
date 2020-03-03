a6449c8df2151237999f27af3685ff39fd887814
João Pedro Quintino <joao.quin@hotmail.com>
2020-03-03 13:35:53 -0300

    O personagem só é retirado da lista de pendentes depois da resposta

--- star_wars_wiki/lib/shared/custom_dio/interceptors.dart
+++ star_wars_wiki/lib/shared/custom_dio/interceptors.dart
@@ -10,29 +10,32 @@ class CustomInterceptors extends InterceptorsWrapper {
   onRequest(RequestOptions options) async {
     print('REQUEST(${options.method}) => PATH: ${options.path}');
     if (options.uri.toString().contains('$FAVS_BASE_URL')) {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-
-      List<String> pendingFavs =
-          prefs.getStringList('pending_favorites') ?? [''];
-
-      String id = options.path
-          .substring('$FAVS_BASE_URL/favorite/'.length, options.path.length);
-      //Remove da lista de favoritos pendentes, caso esteja nela
-      if (pendingFavs.remove(id)) {
-        await prefs.setStringList('pending_favorites', pendingFavs);
-      }
       // Modifica o header Prefer para o valor status=400 em metade das requisições
-      if ((count++) % 2 == 1) {
-        options.headers = {"Prefer": 'status=400'};
+      if (!options.extra['retry']) {
+        if ((count++) % 2 == 1) {
+          options.headers = {"Prefer": 'status=400'};
+        }
       }
     }
     return options;
   }
 
   @override
-  onResponse(Response response) {
+  onResponse(Response response) async {
     print('RESPONSE(${response.statusCode}) => PATH: ${response.request.path}');
-    //print('RESPONSE => ${response.data}');
+    if (response.request.path.contains(FAVS_BASE_URL)) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      List<String> pendingFavs =
+          prefs.getStringList('pending_favorites') ?? [''];
+
+      String id = response.request.path.substring(
+          '$FAVS_BASE_URL/favorite/'.length, response.request.path.length);
+
+      //Remove da lista de favoritos pendentes, caso esteja nela
+      if (pendingFavs.remove(id)) {
+        await prefs.setStringList('pending_favorites', pendingFavs);
+      }
+    }
     return response;
   }
 
@@ -48,10 +51,15 @@ class CustomInterceptors extends InterceptorsWrapper {
         prefs.setStringList('pending_favorites', [id]);
       } else {
         List<String> list = prefs.getStringList('pending_favorites');
-        list.add(id);
-        prefs.setStringList('pending_favorites', list);
+
+        if (!list.contains(id)) {
+          list.add(id);
+
+          prefs.setStringList('pending_favorites', list);
+        }
+        print(list);
       }
-      print(id);
+
       print('Erro: Será feita outra tentativa mais tarde');
     }
     return e;
