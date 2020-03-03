c4576bc1506be7e9504998c2ec942ba3d09d60f5
João Pedro Quintino <joao.quin@hotmail.com>
2020-03-03 08:51:36 -0300

    Usando https na API dos favoritos

--- star_wars_wiki/lib/shared/constants.dart
+++ star_wars_wiki/lib/shared/constants.dart
@@ -1,3 +1,3 @@
 const String BASE_URL = 'https://swapi.co/api';
 const String FAVS_BASE_URL =
-    'http://private-782d3-starwarsfavorites.apiary-mock.com';
+    'https://private-782d3-starwarsfavorites.apiary-mock.com';
