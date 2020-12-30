9d34faf104e54c7ee2a93675b530c1430313cec9
Your Name <null96@outlook.com>
2020-12-30 09:10:16 -0300

    Criado package presentation

--- app/src/main/java/com/example/starwars/presentation/MainActivity.kt
+++ app/src/main/java/com/example/starwars/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.starwars
+package com.example.starwars.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.starwars.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
