a122b892c671ccdce2dd811254b9757e58879a0f
Your Name <null96@outlook.com>
2021-01-01 21:55:42 -0300

    Construindo Estrutura Api Retrofit

---
+++ app/src/main/java/com/example/starwars/data/api/SimpleApi.kt
@@ -0,0 +1,14 @@
+package com.example.starwars.data.api
+
+import com.example.starwars.model.People
+import com.example.starwars.util.Constants.Companion.ENDPOINT_PEOPLE
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface SimpleApi {
+
+    //Endpoint Listar Personagens
+    @GET(ENDPOINT_PEOPLE)
+    suspend fun getPost(): Response<People>
+
+}
