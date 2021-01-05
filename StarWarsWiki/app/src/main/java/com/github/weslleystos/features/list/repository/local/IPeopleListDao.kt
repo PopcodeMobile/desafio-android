a8543782e8c8525beb3b4d01038a2c3e52f4fdd9
weslleystos <weslley_campos@icloud.com>
2021-01-04 21:40:34 -0300

    Criando o Dao de pessoas.

---
+++ StarWarsWiki/app/src/main/java/com/github/weslleystos/features/list/repository/local/IPeopleListDao.kt
@@ -0,0 +1,16 @@
+package com.github.weslleystos.features.list.repository.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.github.weslleystos.shared.entities.People
+
+@Dao
+interface IPeopleListDao {
+    @Query("SELECT * FROM peoples LIMIT :limit OFFSET :offset")
+    fun getAll(offset: Int, limit: Int = 10): List<People>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg peoples: People)
+}
