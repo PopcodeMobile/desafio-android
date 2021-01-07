9912e13700fde18328da461873065e195eee6777
weslleystos <weslley_campos@icloud.com>
2021-01-06 22:40:02 -0300

    Criando a entidade planeta.

---
+++ StarWarsWiki/app/src/main/java/com/github/weslleystos/shared/entities/Planet.kt
@@ -0,0 +1,10 @@
+package com.github.weslleystos.shared.entities
+
+data class Planet(
+    val climate: String,
+    val diameter: String,
+    val gravity: String,
+    val name: String,
+    val population: String,
+    val terrain: String
+)
