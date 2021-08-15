1207c212cb01518f86c06aad742471f0474ce2c2
Guilherme <siedg104@gmail.com>
2021-08-15 17:05:26 -0300

    Created HomeScope.kt and HomeSubComponent.kt

--- app/src/main/java/com/siedg/desafio_android/presentation/di/home/HomeModule.kt
+++ app/src/main/java/com/siedg/desafio_android/presentation/di/home/HomeModule.kt
@@ -1,10 +1,18 @@
 package com.siedg.desafio_android.presentation.di.home
 
+import com.siedg.desafio_android.domain.GetPersonListUseCase
+import com.siedg.desafio_android.presentation.viewmodel.HomeViewModelFactory
 import dagger.Module
 import dagger.Provides
 
 @Module
 class HomeModule {
     @Provides
-    fun provide
+    fun provideHomeViewModelFactory(
+        getPersonListUseCase: GetPersonListUseCase
+    ): HomeViewModelFactory {
+        return HomeViewModelFactory(
+            getPersonListUseCase
+        )
+    }
 }
---
+++ app/src/main/java/com/siedg/desafio_android/presentation/di/home/HomeScope.kt
@@ -0,0 +1,7 @@
+package com.siedg.desafio_android.presentation.di.home
+
+import javax.inject.Scope
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class HomeScope
