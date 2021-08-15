9ac9eba981aac172987e21db1686771f82ad890d
Guilherme <siedg104@gmail.com>
2021-08-15 17:44:05 -0300

    Added fragment_home.xml and home_item.xml

--- app/src/main/java/com/siedg/desafio_android/presentation/di/Injector.kt
+++ app/src/main/java/com/siedg/desafio_android/presentation/di/Injector.kt
@@ -1,4 +1,7 @@
 package com.siedg.desafio_android.presentation.di
 
+import com.siedg.desafio_android.presentation.di.home.HomeSubComponent
+
 interface Injector {
+    fun createHomeSubComponent(): HomeSubComponent
 }
